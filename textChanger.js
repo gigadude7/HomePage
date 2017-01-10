c9124c6d6fa186f85475045479728abbfc7b5e85
Grant Cordle <you@example.com>
2017-01-10 16:59:33 -0600

    - Finished the text reversal script

--- textChanger.js
+++ textChanger.js
@@ -1,3 +1,19 @@
-$("*").mouseup(function() {
-    var clickedElement = this;
+$(document).ready(function() {
+
+	//Click on span
+	$('span').click(function() {
+		this.innerHTML = reversedText(this.innerHTML)
+	}); 
+
+
+	function reversedText(original) {
+		var reversedWordArray = []
+		
+		for (var i = original.length - 1; i >= 0; i--) {
+			reversedWordArray.push(original.charAt(i))
+		};
+
+		return reversedWordArray.join().replace(/,/g, "")
+
+	}
 });
