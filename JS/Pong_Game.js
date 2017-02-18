751100fe10a9b325b01b66cb5f4fe02b3386ba81
Grant <granthc14@gmail.com>
2017-02-18 15:22:06 -0600

    - Just messing around, added a different page for the pong game and changed the title of the main page. Removed the js text changing bc I don't need it right now. Need to figure out how to 'un-underline the link'

---
+++ JS/Pong_Game.js
@@ -0,0 +1,3 @@
+/**
+ * Created by Grant Cordle on 2/15/2017.
+ */
---
+++ JS/Simon_Game.js
@@ -0,0 +1,15 @@
+/**
+ * Created by Grant Cordle on 2/15/2017.
+ */
+
+$(document).ready(function() {
+
+    $("#BackButton").click(function() {
+        window.history.back();
+    })
+
+
+
+
+
+});
